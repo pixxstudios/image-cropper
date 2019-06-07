5daae67b5aab612246f8d80fb0c11a4ee1a3aa03
Gagandeep Singh <gagan.ddun@gmail.com>
2019-06-07 22:11:48 +1000

    file setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as css from './style.css';
+
+const ImageCropper = () => {
+    return (
+        <div className={css.container}>
+            Crop Image
+        </div>
+    )
+}
+
+export default ImageCropper;
